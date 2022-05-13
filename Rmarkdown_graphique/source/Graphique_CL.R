2a05799deb94b60ae0a1c4a84400779930d7ce74
Christophe667 <clenzi@etu.isara.fr>
2022-05-13 14:43:02 +0200

    Graph SW

--- Rmarkdown_graphique/source/Graphique_CL.R
+++ Rmarkdown_graphique/source/Graphique_CL.R
@@ -11,3 +11,5 @@ a <- a + geom_smooth(method = lm, se = FALSE)
 a <- a + theme_light() + ggtitle("Height vs Mass in Star Wars")
 a
 
+# on peut voir un graphique des poids en fonction de la masse des personnages de starwars. 
+# La droite nous permet visualiser plus clairement la relation entre les deux facteurs. 
