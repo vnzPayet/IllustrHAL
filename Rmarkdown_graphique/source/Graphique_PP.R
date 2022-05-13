cf6a6b3f260af366ef032819d92a3fd0ee7b6279
PaulinePerrin <paperrin@etu.isara.fr>
2022-05-13 11:59:47 +0200

    Create Graphique_PP.R

---
+++ Rmarkdown_graphique/source/Graphique_PP.R
@@ -0,0 +1,14 @@
+## Pauline M. Perrin
+
+# Import du jeu de données
+library(nycflights13)
+flights
+library(dplyr)
+
+
+library(tidyverse)
+
+# Graphique des proportions de vols réalisés par chacune des compagnies aériennes étudiées
+ggplot(data=flights) +
+    geom_bar(mapping = aes(x = carrier, y = stat(prop), group = 1))
+
