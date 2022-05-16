fa48f5880e8061c642fc26eb5bc2bec89c0b98e8
GHISLAINE KOSSONOU <gkossonou@etu.isara.fr>
2022-05-16 22:14:00 +0200

    Graphique_GK.R

--- Rmarkdown_graphique/source/Graphique_GK.R
+++ Rmarkdown_graphique/source/Graphique_GK.R
@@ -1,7 +1,8 @@
-
 library(ggplot2)
-
-Papillon <- read.table("Papillon.xlsx", header= TRUE, dec=".", sep=" ",stringsAsFactors = TRUE ) 
+library(readxl)
+Papillon <- read_excel("C:/Users/GHISLAINE KOSSONOU/Desktop/Module D OPEN/OPEN22-KG/TD/OPEN22-graphiques/Rmarkdown_graphique/Papillon.xlsx")
+View(Papillon)
+Papillon <- read.table ("Papillon.xlsx", header= TRUE, dec=".", sep=" ",stringsAsFactors = TRUE ) 
 
 graphGK <- ggplot(data = Papillon) +
   aes(x = Annee_de_derniere_observation, y = Nom_scientifique) +
