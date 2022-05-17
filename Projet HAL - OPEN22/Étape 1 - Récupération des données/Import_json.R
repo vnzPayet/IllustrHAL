237b617d3a7d5961369c691ef5bbbc0eecbcd97e
ClovisBroggio <cbroggio@etu.isara.fr>
2022-05-17 14:26:58 +0200

    Modification du package

--- Projet HAL - OPEN22/Étape 1 - Récupération des données/Import_json.R
+++ Projet HAL - OPEN22/Étape 1 - Récupération des données/Import_json.R
@@ -1,4 +1,5 @@
-library("rjson")
+install.packages("jsonlite")
+library(jsonlite)
 
 url <- "https://api.archives-ouvertes.fr/search/?q=*&fq=(collId_i%3A9626)&start=0&rows=0&wt=json&facet=true&facet.field=submitType_s&facet.sort=count"
 
