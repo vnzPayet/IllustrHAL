f6e68c581b3749991577fdd4dc6af2de3606870f
Christophe667 <clenzi@etu.isara.fr>
2022-05-18 14:57:26 +0200

    renduGCL

--- Rmarkdown_graphique/source/Graphique_CL.R
+++ Rmarkdown_graphique/source/Graphique_CL.R
@@ -5,11 +5,11 @@ dim(starwars)
 
 
 
-a <- ggplot(starwars, aes(height, mass))
-a <- a + geom_point(colour = "red")
-a <- a + geom_smooth(method = lm, se = FALSE)
-a <- a + theme_light() + ggtitle("Height vs Mass in Star Wars")
-a
+graphCL <- ggplot(starwars, aes(height, mass))
+graphCL<- graphCL+ geom_point(colour = "red")+ geom_smooth(method = lm, se = FALSE)
+graphCL<- graphCL + theme_light() + ggtitle("Height vs Mass in Star Wars")
+graphCL
 
+print(graphCL)
 # on peut voir un graphique des poids en fonction de la masse des personnages de starwars. 
 # La droite nous permet visualiser plus clairement la relation entre les deux facteurs. 
