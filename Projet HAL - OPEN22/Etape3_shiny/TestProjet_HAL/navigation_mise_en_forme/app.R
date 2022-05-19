f328cfb661e54d2297d1a96e5381b60e9d79a2cf
Vincent PAYET <vpayet@isara.fr>
2022-05-19 16:41:08 +0200

    Propositions shiny mise en forme

--- Projet HAL - OPEN22/Etape3_shiny/TestProjet_HAL/navigation_mise_en_forme/app.R
+++ Projet HAL - OPEN22/Etape3_shiny/TestProjet_HAL/navigation_mise_en_forme/app.R
@@ -1,59 +1,56 @@
 library(shiny)
-#install.packages("shinythemes")
 library(shinythemes)
 #importation données 
 tableau <- read.csv2("tableau_fictif.txt", header=TRUE, sep=",")
 
 ## UI ###
 ui <- fluidPage(theme=shinytheme("sandstone"),
-                navbarPage("Evolution HALathon",
-                 tabPanel("About page",
-                          h4("Cette page a étée créer dans le contexte HALathon par des étudiants ISARA 4ème année dans le cadre du module optionnel OPEN 2022"
-                          ),
-                          img(src="isara.png", height= 140, width= 400)
-                          ),
-           tabPanel("Documentation"),
-           
-           tabPanel("Application",
-                    sidebarLayout(
-                      sidebarPanel(
-                        tags$head(tags$script(src = "message-handler.js")),
-                        actionButton("do", "Actualisation"),
-                      sidebarPanel(
-                        tags$head(tags$script(src = "message-handler.js")),
-                        actionButton("go", "Graph")
-                      )
-                      ),
-                      
-                      
-                      mainPanel(
-                        tableOutput("table")
-                        #plotOutput("distPlot")
-                      )
-              
-                    )
-)
-)
+    navbarPage("Evolution HALathon",
+               tabPanel("About page",
+                        h4("Cette page a étée créer dans le contexte HALathon par des étudiants ISARA 4ème année dans le cadre du module optionnel OPEN 2022"
+                        ),
+                        img(src="isara.png", width= 300)
+               ),
+               tabPanel("Documentation"),
+               
+               tabPanel("Application",
+                        sidebarLayout(
+                            sidebarPanel(
+                                tags$head(tags$script(src = "message-handler.js")),
+                                actionButton("do", "Actualisation"),
+                                br(),br(),
+                                p("Pour afficher le graph :"),
+                                tags$head(tags$script(src = "message-handler.js")),
+                                actionButton("graph", "Graph")
+                            ),
+                            mainPanel(
+                                tableOutput("table"),
+                                plotOutput("barplot")
+                            )
+                        )
+               )
+    )
 )
 
 
-server <- function(input, output, session) {
+server <- function(input, output) {
   df <- eventReactive(input$do, {
-    #head(tableau, 2)
-    tableau
+    # Requete HAL et traitement json...
+      tail(tableau, 3)
   })
+  
+  ## render texte ?
   output$table <- renderTable({
     df()
   })
   
-  gp <- eventReactive(input$go,{
+  gp <- eventReactive(input$graph, {
     tableau
   })
   
-  output$table <- renderTable({
-    gp()
+    output$barplot <- renderPlot({
+      plot(gp()$notice, type="p")
   })
-  
 }
 
 
