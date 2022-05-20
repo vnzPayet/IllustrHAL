f3f5c89ec605208e1e23487a3058b91c38e299ba
ClovisBroggio <cbroggio@etu.isara.fr>
2022-05-20 08:10:14 +0200

    Boucle for qui créer tous les graphiques

--- Projet HAL - OPEN22/Etape2_ representation_ graphique _et_ animation/Code_graphique.R
+++ Projet HAL - OPEN22/Etape2_ representation_ graphique _et_ animation/Code_graphique.R
@@ -3,8 +3,8 @@
 # Tableau de données : "tableau"
 # Noms des colonnes : "date", "notice", "fichier"
 
-# Importer le tableau à partir d'un fichir csv
-tableau <- read.table("tableau_fictif.csv", sep = ",", header = TRUE)
+# Importer le tableau à partir d'un fichir txt
+tableau <- read.table("tableau_fictif.txt", sep = ",", header = TRUE)
 dim(tableau)
 
 names(tableau)
@@ -16,53 +16,44 @@ as_tibble(tableau)
 
 # Graphiques
 
-n(tableau)
+longueur_tableau <- nrow(tableau)
 
-notice_premiere_ligne <- tableau %>%
-  slice(1)%>%
+notice_derniere_ligne <- tableau %>%
+  slice(longueur_tableau)%>%
   select(notice)
-file_premiere_ligne <- tableau %>%
-  slice(1)%>%
+
+file_derniere_ligne <- tableau %>%
+  slice(longueur_tableau)%>%
   select(file)
 
-n <- tableau %>%
-  count(tableau)
+notice_derniere_ligne <- as.integer(notice_derniere_ligne)
+file_derniere_ligne <- as.integer(file_derniere_ligne)
+
+maxi <- max(c(file_derniere_ligne,notice_derniere_ligne))
 
 
-as.numeric(notice_premiere_ligne)
-as.numeric(file_premiere_ligne)
-
-
-maxi <- max(c(200,1000))*1.2
-
-barplot(c(file_premiere_ligne,notice_premiere_ligne), 
-        names.arg = c("File", "Notice"),
-        main="Représentation graphique du nombre de dépôt en fonction du type de publication",
-        xlab="Type de publication",
-        ylab="Nombre de dépôts",
-        col=c("cadetblue3", "sienna1"),
-        las=1,
-        ylim=c(0,maxi))
-
-barplot(c(100,150), 
-        names.arg = c("File", "Notice"),
-        main="Représentation graphique du nombre de dépôt en fonction du type de publication",
-        xlab="Type de publication",
-        ylab="Nombre de dépôts",
-        col=c("cadetblue3", "sienna1"),
-        las=1,
-        ylim=c(0,maxi))
-
-png("Test.png", width = 400, height = 480)
-barplot(c(200,300), 
-        names.arg = c("File", "Notice"),
-        main="Représentation graphique du nombre de dépôt en fonction du type de publication",
-        xlab="Type de publication",
-        ylab="Nombre de dépôts",
-        col=c("cadetblue3", "sienna1"),
-        las=1,
-        ylim=c(0,maxi))
-dev.off()
-
-abline(h=maxi, col="grey")
-
+for (i in 1:longueur_tableau){
+  notice_premiere_ligne <- tableau %>%
+    slice(i)%>%
+    select(notice)
+  file_premiere_ligne <- tableau %>%
+    slice(i)%>%
+    select(file)
+  
+  notice_premiere_ligne <- as.integer(notice_premiere_ligne)
+  file_premiere_ligne <- as.integer(file_premiere_ligne)
+  
+  
+  png(str_glue(str_glue("Graphique n°{i}.png")), width = 400, height = 480)
+  barplot(c(file_premiere_ligne, notice_premiere_ligne), 
+          names.arg = c("File", "Notice"),
+          main="Représentation graphique du nombre de dépôt en fonction du type de publication",
+          xlab="Type de publication",
+          ylab="Nombre de dépôts",
+          col=c("cadetblue3", "sienna1"),
+          las=1,
+          ylim=c(0,maxi))
+  dev.off()
+  }
+  
+  
