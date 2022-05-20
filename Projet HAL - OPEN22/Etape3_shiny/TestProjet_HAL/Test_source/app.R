a2f2fca82a97202b55525d6152280d7abf3d7fe6
Hélène Vignon <helenevignon@MacBook-Air-de-Helene.local>
2022-05-20 12:54:51 +0200

    modification fichier source tabAbout

--- Projet HAL - OPEN22/Etape3_shiny/TestProjet_HAL/Test_source/app.R
+++ Projet HAL - OPEN22/Etape3_shiny/TestProjet_HAL/Test_source/app.R
@@ -8,28 +8,9 @@ ui <- fluidPage(theme=shinytheme("sandstone"),
                 navbarPage("Evolution HALathon",
                            tabPanel("Accueil", source("tabAccueil.R", local = TRUE)$value),
                            tabPanel("Application", source("tabApp.R", local = TRUE)$value),
-                           tabPanel("About page", 
-                                     h4("Cette page a été créer dans le contexte du",strong('HALathon'),", par des étudiants de", a(href = "https://isara.fr","l'ISARA"), "de 4ème année, dans le cadre de leur module optionnel OPEN (Outils, Perspectives et Enjeux du Numériques) 2022."),
-                                     h4("La Source est à l'origine de cette demande."),
-                                     h4("Le commanditaire est",strong('Oriane Debiez'),"documentaliste et représentant Les Sourcies, équipe du Centre de documentation de l'ISARA."),
-                                     h4("L'accompagnement des élèves sur ce projet a été fait par",strong("Vincent PAYET"),", enseignant de l'ISARA."),
-                                     h4("Les étudiants qui ont participés sont :"),
-                                     h4(em("• Clovis BROGGIO")),
-                                     h4(em("• Léa DANIELI")),
-                                     h4(em("• Jeanne HECKENBENNER")),
-                                     h4(em("• Ghislaine KOSSONOU")),
-                                     h4(em("• Manon LAFONT")),
-                                     h4(em("• Christophe LENZI")),
-                                     h4(em("• Pauline M PERRIN")),
-                                     h4(em("• Léa SAGNE")),
-                                     h4(em("• Hélène VIGNON")),
-                                     img(src = "isara.png", width = 300),
-                                     img(src = "hal.png", width = 300),
-                                     ),
-                            
+                           tabPaneltabPanel("About page",source("tabAbout.R", local = TRUE)$value),
                            )
                 )
-
 
 # faire un bouton reset ???
 # faire le rendertexte 
