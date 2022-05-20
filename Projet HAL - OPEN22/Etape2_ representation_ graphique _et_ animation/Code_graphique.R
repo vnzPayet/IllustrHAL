536de095212c313629c3978baf165d7a8e4b3d53
Vincent PAYET <vpayet@isara.fr>
2022-05-20 10:15:05 +0200

    modif boucle for

--- Projet HAL - OPEN22/Etape2_ representation_ graphique _et_ animation/Code_graphique.R
+++ Projet HAL - OPEN22/Etape2_ representation_ graphique _et_ animation/Code_graphique.R
@@ -6,31 +6,31 @@
 # Importer le tableau à partir d'un fichir txt
 tableau <- read.table("tableau_fictif.txt", sep = ",", header = TRUE)
 dim(tableau)
-
 names(tableau)
 
 
 library(tidyverse)
 library(dplyr)
-as_tibble(tableau)
+#tableau <- 
+tableau <- as_tibble(tableau)
 
 # Graphiques
-
 longueur_tableau <- nrow(tableau)
 
 notice_derniere_ligne <- tableau %>%
   slice(longueur_tableau)%>%
   select(notice)
 
+## Sélection de la dernière valeur de la colonne file
 file_derniere_ligne <- tableau %>%
   slice(longueur_tableau)%>%
   select(file)
 
+## Les dernière valeurs en nombre simple
 notice_derniere_ligne <- as.integer(notice_derniere_ligne)
 file_derniere_ligne <- as.integer(file_derniere_ligne)
 
 maxi <- max(c(file_derniere_ligne,notice_derniere_ligne))
-
 
 for (i in 1:longueur_tableau){
   notice_premiere_ligne <- tableau %>%
@@ -44,7 +44,9 @@ for (i in 1:longueur_tableau){
   file_premiere_ligne <- as.integer(file_premiere_ligne)
   
   
-  png(str_glue(str_glue("Graphique n°{i}.png")), width = 400, height = 480)
+ # png(str_glue(str_glue("Graphique n°{i}.png")), width = 400, height = 480)
+  png(str_glue(str_glue("fig/Graphique_{i+100}.png")), width = 400, height = 480)
+  par(mar=c(4,4,4,10))
   barplot(c(file_premiere_ligne, notice_premiere_ligne), 
           names.arg = c("File", "Notice"),
           main="Représentation graphique du nombre de dépôt en fonction du type de publication",
@@ -53,6 +55,7 @@ for (i in 1:longueur_tableau){
           col=c("cadetblue3", "sienna1"),
           las=1,
           ylim=c(0,maxi))
+  abline(h=200, col="grey", lwd=2)
   dev.off()
   }
   
